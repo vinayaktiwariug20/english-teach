45382b976dcdd21a2d042ba086eb0f89b22a4008
Vinayak Tiwari <vinayak.tiwari.ug20@gmail.com>
2026-09-20 12:22:20 -0700

    Bump service worker cache to v30

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // network again, which is what makes it usable with no signal and guarantees
 // nothing can ever inject an ad into it.
 
-const CACHE = 'english-teach-v29';
+const CACHE = 'english-teach-v30';
 
 const ASSETS = [
   './',
